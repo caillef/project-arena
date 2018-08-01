8a802d469ea611273b7c1bd74114de2ab1f423bf
Corentin CAILLEAUD <caille_f@epitech.eu>
2018-08-01 08:07:39 +0200

    Add highlight when mouse goes on a cell

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,11 @@ const SCREEN_HEIGHT = 720
 const MAP_HEIGHT = 12
 const MAP_WIDTH = 12
 
+const ORIGIN = {
+    X: SCREEN_WIDTH / 2,
+    Y: SCREEN_HEIGHT / 6
+}
+
 const config = {
     type: Phaser.AUTO,
     width: SCREEN_WIDTH,
@@ -12,7 +17,8 @@ const config = {
     parent: 'Project Arena',
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 const game = new Phaser.Game(config);
@@ -42,14 +48,14 @@ function preload() {
 const map = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [2, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0],
+    [2, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0],
+    [2, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [2, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0],
+    [2, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0],
+    [2, 0, 0, 1, 0, 0, 0, 0, 0, -1, 0, 0],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
@@ -70,17 +76,56 @@ const map_textures = [
 ]
 
 function create() {
+    initializeMap(this)
+    initializePlayer(this)
+}
+
+function update() {
+    if (game.input.activePointer.isDown) {
+        console.log("plop")
+    }
+}
+
+function initializePlayer(self) {
+    const pos = {x: 5, y:5, z:0}
+    const isopos = convert3dToIsometric(pos)
+    self.player = {
+        pos,
+        sprite: self.add.sprite(SCREEN_WIDTH / 2 + isopos.x * (83 / 2), SCREEN_HEIGHT / 6 + isopos.y * (96 / 2), 'player')
+    }
+}
+
+function initializeMap(self) {
+    self.map = {
+        cells: []
+    };
     for (y in map) {
         const row = map[y];
         for (x in row) {
             const z = row[x]
             for (let i = 0; i <= z; i++) {
-                const isopos = convert3dToIsometric({
+                const pos = {
                     x: parseInt(x),
                     y: parseInt(y),
                     z: parseInt(i)
-                })
-                this.add.sprite(SCREEN_WIDTH / 2 + isopos.x * (83 / 2), SCREEN_HEIGHT / 6 + isopos.y * (96 / 2), 'tile' + map_textures[y][x]);    
+                }
+                if (pos.z == -1)
+                    continue;
+                const isopos = convert3dToIsometric(pos)
+                const cell = {
+                    pos,
+                    sprite: self.add.sprite(ORIGIN.X + isopos.x * (83 / 2), ORIGIN.Y + isopos.y * (96 / 2), 'tile' + map_textures[y][x]).setInteractive()
+                }
+                cell.sprite.tint = 0xdddddd;
+                self.map.cells.push(cell)
+                cell.sprite.on('pointerover', (_) => {
+                    self.selectedCell = cell
+                    cell.sprite.tint = 0xffffff;
+                });
+                cell.sprite.on('pointerout', (_) => {
+                    cell.sprite.tint = 0xdddddd;
+                });
+                cell.sprite.input.pixelPerfectOver = true;
             }
         }
     }
